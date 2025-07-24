f51e3a39b10273a5d5b1c69a34650eed84ad49c1
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-07-24 19:21:40 +0200

    Refactor API routes: replace individual route inclusions with `DefaultRouter`, register viewsets for ingredients, tags, recipes, and users.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,20 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from recipes.views import IngredientViewSet, TagViewSet, RecipeViewSet
+from users.views import CustomUserViewSet
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('tags', TagViewSet, basename='tags')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('users', CustomUserViewSet, basename='users')
+
 
 urlpatterns = [
-    path('', include('users.urls')),
-    path('', include('recipes.urls')),
-    path('auth/', include('djoser.urls')),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
