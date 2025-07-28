1358a547e0f6780b81753c8c407b66a189d50ac2
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-07-28 22:29:30 +0200

    Rename `CustomPagination` to `CustomUserPagination` for clarity.

--- backend/recipes/pagination.py
+++ backend/recipes/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-class CustomPagination(PageNumberPagination):
+class CustomUserPagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_query_param = 'page'
 
