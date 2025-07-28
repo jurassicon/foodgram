53b002166a5175b36ccd9494b3eb703499d989ae
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-07-28 18:22:33 +0200

    Refactor `RecipeFilter`: rewrite `is_favorited` filter for accuracy, move `Meta` class, and improve queryset handling for authenticated users.

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -26,23 +26,31 @@ class RecipeFilter(FilterSet):
         field_name='tags__slug',
         to_field_name='slug'
     )
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_favorited = filters.BooleanFilter(
+        field_name='favorites__user',
+        lookup_expr='exact'
+    )
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_in_shopping_list'
     )
 
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
-        if value and user.is_authenticated:
-            return queryset.filter(favorites__user_id=user.id)
-        return queryset
+        if not user.is_authenticated:
+            return queryset.none()
+        if value:
+            return (
+                queryset
+                .filter(favorites__user=user)
+            )
+        return queryset.exclude(favorites__user=user)
 
     def filter_in_shopping_list(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(shopping_recipe__user_id=user.id)
         return queryset
-
-    class Meta:
-        model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
