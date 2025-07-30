accd1f7ef7ef8d48c0201368637e9082af59e97e
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-07-30 20:49:37 +0200

    Add missing blank line in `users/validators.py` for PEP 8 compliance.

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -2,6 +2,7 @@ from django.core import exceptions
 
 forbidden_usernames = ['me', 'admin', 'Gordon Ramsay', 'Андрей Макаревич']
 
+
 def validate_username(value):
     normalized = value.strip().lower()
     forbidden_normalized = [name.lower() for name in forbidden_usernames]
