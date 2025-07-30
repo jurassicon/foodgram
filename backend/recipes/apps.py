1a7fd1b6ff66fbebfd4469d2e594f07e85151094
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-07-30 20:50:13 +0200

    Remove temporary admin creation logic from `RecipesConfig` class

--- backend/recipes/apps.py
+++ backend/recipes/apps.py
@@ -1,31 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 
 class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
-
-    # !!! Удалить перед продом!!!
-    def ready(self):
-        from django.contrib.auth import get_user_model
-
-        def create_admin(sender, **kwargs):
-            User = get_user_model()
-            username = 'cherkasov'
-            email = 'admin@example.com'
-            password = 'JURAssic'
-
-            # либо найдём, либо создадим пользователя
-            user, created = User.objects.get_or_create(
-                username=username,
-                defaults={'email': email}
-            )
-            if created:
-                # если только что создали — сделаем его суперюзером
-                user.set_password(password)
-                user.is_staff = True
-                user.is_superuser = True
-                user.save()
-
-        post_migrate.connect(create_admin, sender=self)
