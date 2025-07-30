bae8fe2b6f094efc18f5fbe178ccf3eb0619f253
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-07-30 20:19:07 +0200

    Add missing blank line in `pagination.py` for PEP 8 compliance.

--- backend/recipes/pagination.py
+++ backend/recipes/pagination.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
+
 class CustomUserPagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_query_param = 'page'
