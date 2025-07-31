5ca16a05b14cf13dffbe3ffefaf7f5742cced8a1
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-08-01 01:47:57 +0200

    Update database environment variables in Django settings for consistency and clarity

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,13 +72,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'NAME': os.getenv('DB_NAME', 'foodgram'),
+        'USER': os.getenv('DB_USER', 'foodgram_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
