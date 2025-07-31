2f52924907940e541becf254d0960afbc756e71b
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-07-31 18:34:53 +0200

    Refactor `recipes.signals` to improve readability by adjusting spacing and imports.

--- backend/recipes/signals.py
+++ backend/recipes/signals.py
@@ -1,11 +1,14 @@
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
+
 from .models import Recipe
+
 
 @receiver(pre_delete, sender=Recipe)
 def delete_recipe_image(sender, instance: Recipe, **kwargs):
     if instance.image:
         instance.image.delete(save=False)
+
 
 @receiver(pre_save, sender=Recipe)
 def delete_old_recipe_image(sender, instance: Recipe, **kwargs):
