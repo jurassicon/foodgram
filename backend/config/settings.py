5aa33671b46451d96a141ab0e0959055b4c3d2d7
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-08-01 17:16:41 +0200

    Update database environment variable names in Django settings for better alignment with standard conventions

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,11 +72,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'foodgram'),
-        'USER': os.getenv('DB_USER', 'foodgram_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', 5432),
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
