b3fd88d6c659c89be581f725f4ea09dc6679037c
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-08-06 23:27:19 +0200

    Reorder authentication classes in REST framework settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,8 +132,8 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'recipes.pagination.RecipesPagination',
