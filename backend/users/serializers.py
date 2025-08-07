c6737d98082cd977d94db2bf5ba2c3e0e85ccaac
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-08-07 22:56:27 +0200

    fix relative import

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,7 +2,7 @@ from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from recipes.models import Recipe
-from .models import Follow, User
+from users.models import Follow, User
 
 
 class AvatarSerializer(serializers.ModelSerializer):
