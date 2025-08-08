eb10d7c7afe7946425866f96d9ead8660deedbad
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-08-08 16:27:11 +0200

    use settings constant for recipes pagination

--- backend/recipes/pagination.py
+++ backend/recipes/pagination.py
@@ -1,6 +1,8 @@
 from rest_framework.pagination import PageNumberPagination
 
+from config import settings
+
 
 class RecipesPagination(PageNumberPagination):
-    page_size = 6
+    page_size = settings.RECIPES_PER_PAGE
     page_size_query_param = 'limit'
